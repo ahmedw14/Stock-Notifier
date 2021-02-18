2c64b506e117ab6d884d08d1537b1c854abf7428
ahmedw14 <67707288+ahmedw14@users.noreply.github.com>
2021-02-17 18:58:02 -0700

    Add files via upload

---
+++ company.py
@@ -0,0 +1,37 @@
+class company:
+    def __init__(self, name, ticker, price,eps, growth, high, low):
+        self.name =name
+        self.ticker=ticker
+        self.price =price
+        self.eps =eps
+        self.growth =growth
+        self.high =high
+        self.low =low
+company1=[]
+company1.append(company("Facebook", "FB",0,0,0,0,0))
+company1.append(company("Apple", "AAPL",0,0,0,0,0))
+company1.append(company("Microsoft", "MSFT",0,0,0,0,0))
+company1.append(company("Amazon", "AMZN",0,0,0,0,0))
+company1.append(company("Bank of America", "BAC",0,0,0,0,0))
+company1.append(company("Square", "SQ",0,0,0,0,0))
+company1.append(company("Intel", "INTC",0,0,0,0,0))
+company1.append(company("Pepsico", "PEP",0,0,0,0,0))
+company1.append(company("Tesla", "TSLA",0,0,0,0,0))
+company1.append(company("Toronto Dominian Bank", "TD.TO",0,0,0,0,0))
+company1.append(company("Walt Disney Co", "DIS",0,0,0,0,0))
+company1.append(company("Nike", "NKE",0,0,0,0,0))
+company1.append(company("Walt Disney Co", "DIS",0,0,0,0,0))
+company1.append(company("The Coca-Cola Company", "KO",0,0,0,0,0))
+company1.append(company("3M", "MMM",0,0,0,0,0))
+company1.append(company("AT&T", "T",0,0,0,0,0))
+company1.append(company("Visa", "V",0,0,0,0,0))
+company1.append(company("Mastercard", "MA",0,0,0,0,0))
+company1.append(company("Paypal", "PYPL",0,0,0,0,0))
+company1.append(company("Air Canada", "AC.TO",0,0,0,0,0))
+company1.append(company("Suncore Energy", "SU.TO",0,0,0,0,0))
+company1.append(company("Canadian National Railway", "CNR.TO",0,0,0,0,0))
+company1.append(company("Canadian Tire", "CTC-A.TO",0,0,0,0,0))
+company1.append(company("Google", "GOOG",0,0,0,0,0))
+company1.append(company("American Express", "AXP",0,0,0,0,0))
+company1.append(company("Wells Fargo", "WFC",0,0,0,0,0))
+company1.append(company("Real Matters", "ALA.TO",0,0,0,0,0))
